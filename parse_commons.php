9e3ced3ad83d7e05ff933e4f2ae83f81819cee8c
Roberto Guido <bob4job@gmail.com>
2016-06-13 01:04:43 +0200

    aggiunta visualizzazione distribuzione assoluta; spostata computazione dei valori client-side

--- parse_commons.php
+++ parse_commons.php
@@ -9,7 +9,6 @@ function handle_data($file, $elements) {
 
   foreach($elements as $name => $info) {
     $info->votes = array_fill_keys([1, 2, 3, 4, 5, 6, 7, 8], 0);
-    $info->shade_relative = array_fill_keys([1, 2, 3, 4, 5, 6, 7, 8], 0);
   }
 
   /*
@@ -31,28 +30,6 @@ function handle_data($file, $elements) {
 
       $info->votes[$circ] += $row[$info->col];
     }
-  }
-
-  /*
-    Calcolo dell'incidenza relativa
-  */
-
-  $master = $elements['VOTANTI'];
-  $master->shade_relative = array_fill_keys([1, 2, 3, 4, 5, 6, 7, 8], 255);
-  $master = $master->votes;
-
-  foreach($elements as $name => $info) {
-    if ($name == 'VOTANTI')
-      continue;
-
-    $max = 0;
-
-    foreach($info->votes as $circ => $v)
-      if ($master[$circ] / $v > $max)
-        $max = $v;
-
-    foreach($info->votes as $index => $v)
-      $info->shade_relative[$index] = floor(($v * 255) / $max);
   }
 
   return $elements;
